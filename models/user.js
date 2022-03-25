7c79cc906ad880615803191847b5337f4b33c6c3
tristonpalacios <triston.j.palacios@gmail.com>
2022-03-25 11:54:58 -0700

    Rough Schema Mockup

--- models/user.js
+++ models/user.js
@@ -3,7 +3,27 @@ const mongoose = require('mongoose')
 const UserSchema = mongoose.Schema({
   name: String,
   email: String,
-  password: String
+  password: String,
+  EventAttending: { type: Schema.Types.Event, ref: 'Event' },
 })
+
+const EventSchema = mongoose.Schema({
+  title: String,
+  City:String,
+  State:String,
+  Zipcode: Number,
+  Date: Date,
+  Description: String,
+  Category: String,
+  Attendees:[{ type: Schema.Types.UserId, ref: 'user' }],
+  Host: {type: Schema.Types.UserId, ref:'user'}
+})
+
+const AttendeSchema = mongoos.Schema({
+  Attendees: ,
+  EventId:
+})
+
+
 
 module.exports = mongoose.model('User', UserSchema)
